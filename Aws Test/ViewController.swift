d8e4a511229f932ba97db0f27b7ef214f894e21e
Mohit Andhare <mohitandhare@skromanglobal.com>
2022-07-26 16:59:54 +0530

    Initial Commit

---
+++ Aws Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aws Test
+//
+//  Created by Developer Skromanglobal on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

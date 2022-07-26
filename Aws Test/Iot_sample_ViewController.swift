05dcc3406f29aa8f7a3d543c6cb33c1a0f02765b
Mohit Andhare <mohitandhare@skromanglobal.com>
2022-07-26 18:39:46 +0530

    AWS Pub Sub

---
+++ Aws Test/Iot_sample_ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Iot_sample_ViewController.swift
+//  Aws Test
+//
+//  Created by Developer Skromanglobal on 26/07/22.
+//
+
+import UIKit
+
+class Iot_sample_ViewController: UIViewController {
+
+    
+    @objc var mqttStatus: String = "Disconnected"
+    @objc var topic: String = "SKSL_1xGLn8/HA/E/ack"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- Aws Test/ViewController.swift
+++ Aws Test/ViewController.swift
@@ -6,14 +6,436 @@
 //
 
 import UIKit
+import AWSCore
+import AWSIoT
+import Alamofire
 
 class ViewController: UIViewController {
 
+    struct test_aws : Decodable {
+        
+        let control : String
+        let no : Int
+        let state : Int
+        let speed : Int
+      
+    }
+    
+    
+    
+    
+    let IOT_CERT = "IoT Cert"
+    let IOT_WEBSOCKET = "IoT Websocket"
+
+    var connectIoTDataWebSocket: UIButton!
+    var activityIndicatorView: UIActivityIndicatorView!
+   
+    var logTextView: UITextView!
+    var connectButton: UIButton!
+    
+    var connected = false
+    
+    
+    var iotDataManager: AWSIoTDataManager!
+    var iotManager: AWSIoTManager!
+    var iot: AWSIoT!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:AWS_REGION,
+                                                                identityPoolId:IDENTITY_POOL_ID)
+        initializeControlPlane(credentialsProvider: credentialsProvider)
+        initializeDataPlane(credentialsProvider: credentialsProvider)
+      
+    }
+    
+
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        let iotDataManager = AWSIoTDataManager(forKey: AWS_IOT_DATA_MANAGER_KEY)
+        
+        let iot_sample_vc = Iot_sample_ViewController()
+        
+        iotDataManager.unsubscribeTopic(iot_sample_vc.topic)
+    }
+    
+    
+    
+      @IBAction func connect_button(_ sender: UIButton) {
+        
+        
+        sender.isEnabled = false
+        if (connected == false) {
+            handleConnectViaCert()
+        } else {
+            handleDisconnect()
+            DispatchQueue.main.async {
+                sender.setTitle("Connect \(self.IOT_CERT)", for:UIControl.State())
+                sender.isEnabled = true
+            }
+        }
+        
+    }
+    
+    
+    
+    @IBAction func pub_button(_ sender: UIButton) {
+        
+        let aws_params : Parameters = [
+        
+            "control": "L",
+            "no" : 1,
+            "state" : 1,
+            "speed" : 1
+            
+            
+        ]
+        
+        let iotDataManager = AWSIoTDataManager(forKey: AWS_IOT_DATA_MANAGER_KEY)
+       
+        let iot_sample_vc = Iot_sample_ViewController()
+
+//        iotDataManager.publishString("\(aws_post())", onTopic:iot_sample_vc.topic, qoS:.messageDeliveryAttemptedAtMostOnce)
+
+        
+        iotDataManager.publishString("\(aws_params.self)", onTopic:iot_sample_vc.topic, qoS:.messageDeliveryAttemptedAtMostOnce)
+        
+    }
+    
+    
+    
+    
+    @IBAction func sub_button(_ sender: UIButton) {
+        
+        let iotDataManager = AWSIoTDataManager(forKey: AWS_IOT_DATA_MANAGER_KEY)
+       
+        let ios_sample_vc = Iot_sample_ViewController()
+
+        iotDataManager.subscribe(toTopic: ios_sample_vc.topic, qoS: .messageDeliveryAttemptedAtMostOnce, messageCallback: {
+            (payload) ->Void in
+            let stringValue = NSString(data: payload, encoding: String.Encoding.utf8.rawValue)!
+
+            print("received: \(stringValue)")
+
+        })
+        
+        
+    }
+    
+    
+    func mqttEventCallback( _ status: AWSIoTMQTTStatus ) {
+        DispatchQueue.main.async {
+            let iot_sample_vc = Iot_sample_ViewController()
+            print("connection status = \(status.rawValue)")
+
+            switch status {
+            case .connecting:
+                iot_sample_vc.mqttStatus = "Connecting..."
+                print( iot_sample_vc.mqttStatus )
+//                self.logTextView.text = iot_sample_vc.mqttStatus
+                
+            case .connected:
+                iot_sample_vc.mqttStatus = "Connected"
+                print( iot_sample_vc.mqttStatus )
+//                self.connectButton.setTitle( "Disconnect \(self.IOT_CERT)", for:UIControl.State())
+//                self.activityIndicatorView.stopAnimating()
+                self.connected = true
+//                self.connectButton.isEnabled = true
+                let uuid = UUID().uuidString;
+                let defaults = UserDefaults.standard
+                let certificateId = defaults.string( forKey: "certificateId")
+                
+//                self.logTextView.text = "Using certificate:\n\(certificateId!)\n\n\nClient ID:\n\(uuid)"
+               
+                
+            case .disconnected:
+                iot_sample_vc.mqttStatus = "Disconnected"
+                print( iot_sample_vc.mqttStatus )
+//                self.activityIndicatorView.stopAnimating()
+//                self.logTextView.text = nil
+                
+            case .connectionRefused:
+                iot_sample_vc.mqttStatus = "Connection Refused"
+                print( iot_sample_vc.mqttStatus )
+//                self.activityIndicatorView.stopAnimating()
+//                self.logTextView.text = iot_sample_vc.mqttStatus
+                
+            case .connectionError:
+                iot_sample_vc.mqttStatus = "Connection Error"
+                print( iot_sample_vc.mqttStatus )
+//                self.activityIndicatorView.stopAnimating()
+//                self.logTextView.text = iot_sample_vc.mqttStatus
+                
+            case .protocolError:
+                iot_sample_vc.mqttStatus = "Protocol Error"
+                print( iot_sample_vc.mqttStatus )
+//                self.activityIndicatorView.stopAnimating()
+//                self.logTextView.text = iot_sample_vc.mqttStatus
+                
+            default:
+                iot_sample_vc.mqttStatus = "Unknown State"
+                print("unknown state: \(status.rawValue)")
+//                self.activityIndicatorView.stopAnimating()
+//                self.logTextView.text = iot_sample_vc.mqttStatus
+            }
+            
+            NotificationCenter.default.post( name: Notification.Name(rawValue: "connectionStatusChanged"), object: self )
+        }
+    }
+    
+    
+    
+    
+    
+    
+    func handleConnectViaCert() {
+//        self.connectIoTDataWebSocket.isHidden = true
+//        activityIndicatorView.startAnimating()
+        
+        let defaults = UserDefaults.standard
+        let certificateId = defaults.string( forKey: "certificateId")
+        if (certificateId == nil) {
+            DispatchQueue.main.async {
+//                self.logTextView.text = "No identity available, searching bundle..."
+            }
+            let certificateIdInBundle = searchForExistingCertificateIdInBundle()
+            
+            if (certificateIdInBundle == nil) {
+                DispatchQueue.main.async {
+//                    self.logTextView.text = "No identity found in bundle, creating one..."
+                }
+                createCertificateIdAndStoreinNSUserDefaults(onSuccess: {generatedCertificateId in
+                    let uuid = UUID().uuidString
+//                    self.logTextView.text = "Using certificate: \(generatedCertificateId)"
+                    self.iotDataManager.connect( withClientId: uuid, cleanSession:true, certificateId:generatedCertificateId, statusCallback: self.mqttEventCallback)
+                }, onFailure: {error in
+                    print("Received error: \(error)")
+                })
+            }
+        } else {
+            let uuid = UUID().uuidString;
+            // Connect to the AWS IoT data plane service w/ certificate
+            iotDataManager.connect( withClientId: uuid, cleanSession:true, certificateId:certificateId!, statusCallback: self.mqttEventCallback)
+        }
     }
 
+    func searchForExistingCertificateIdInBundle() -> String? {
+        let defaults = UserDefaults.standard
+        // No certificate ID has been stored in the user defaults; check to see if any .p12 files
+        // exist in the bundle.
+        let myBundle = Bundle.main
+        let myImages = myBundle.paths(forResourcesOfType: "p12" as String, inDirectory:nil)
+        let uuid = UUID().uuidString
 
+        guard let certId = myImages.first else {
+            let certificateId = defaults.string(forKey: "certificateId")
+            return certificateId
+        }
+      
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: certId)) else {
+            print("[ERROR] Found PKCS12 File in bundle, but unable to use it")
+            let certificateId = defaults.string( forKey: "certificateId")
+            return certificateId
+        }
+        
+        DispatchQueue.main.async {
+            self.logTextView.text = "found identity \(certId), importing..."
+        }
+        if AWSIoTManager.importIdentity( fromPKCS12Data: data, passPhrase:"", certificateId:certId) {
+            
+            defaults.set(certId, forKey:"certificateId")
+            defaults.set("from-bundle", forKey:"certificateArn")
+            DispatchQueue.main.async {
+                self.logTextView.text = "Using certificate: \(certId))"
+                self.iotDataManager.connect( withClientId: uuid,
+                                             cleanSession:true,
+                                             certificateId:certId,
+                                             statusCallback: self.mqttEventCallback)
+            }
+        }
+        
+        let certificateId = defaults.string( forKey: "certificateId")
+        return certificateId
+    }
+
+    func createCertificateIdAndStoreinNSUserDefaults(onSuccess:  @escaping (String)->Void,
+                                                     onFailure: @escaping (Error) -> Void) {
+        let defaults = UserDefaults.standard
+        // Now create and store the certificate ID in NSUserDefaults
+        let csrDictionary = [ "commonName": CertificateSigningRequestCommonName,
+                              "countryName": CertificateSigningRequestCountryName,
+                              "organizationName": CertificateSigningRequestOrganizationName,
+                              "organizationalUnitName": CertificateSigningRequestOrganizationalUnitName]
+        
+        self.iotManager.createKeysAndCertificate(fromCsr: csrDictionary) { (response) -> Void in
+            guard let response = response else {
+                DispatchQueue.main.async {
+                    self.connectButton.isEnabled = true
+                    self.activityIndicatorView.stopAnimating()
+                    self.logTextView.text = "Unable to create keys and/or certificate, check values in Constants.swift"
+                }
+                onFailure(NSError(domain: "No response on iotManager.createKeysAndCertificate", code: -2, userInfo: nil))
+                return
+            }
+            defaults.set(response.certificateId, forKey:"certificateId")
+            defaults.set(response.certificateArn, forKey:"certificateArn")
+            let certificateId = response.certificateId
+            print("response: [\(String(describing: response))]")
+            
+            let attachPrincipalPolicyRequest = AWSIoTAttachPrincipalPolicyRequest()
+            attachPrincipalPolicyRequest?.policyName = POLICY_NAME
+            attachPrincipalPolicyRequest?.principal = response.certificateArn
+            
+            // Attach the policy to the certificate
+            self.iot.attachPrincipalPolicy(attachPrincipalPolicyRequest!).continueWith (block: { (task) -> AnyObject? in
+                if let error = task.error {
+                    print("Failed: [\(error)]")
+                    onFailure(error)
+                } else  {
+                    print("result: [\(String(describing: task.result))]")
+                    DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+                        if let certificateId = certificateId {
+                            onSuccess(certificateId)
+                        } else {
+                            onFailure(NSError(domain: "Unable to generate certificate id", code: -1, userInfo: nil))
+                        }
+                    })
+                }
+                return nil
+            })
+        }
+    }
+
+    func handleDisconnect() {
+        
+        let iot_sample_vc = Iot_sample_ViewController()
+        self.connectButton.isHidden = false
+        self.connectIoTDataWebSocket.isHidden = false
+        activityIndicatorView.startAnimating()
+        logTextView.text = "Disconnecting..."
+        
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+            self.iotDataManager.disconnect();
+            DispatchQueue.main.async {
+                self.connected = false
+               
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    func initializeControlPlane(credentialsProvider: AWSCredentialsProvider) {
+        
+        let controlPlaneServiceConfiguration = AWSServiceConfiguration(region:AWS_REGION, credentialsProvider:credentialsProvider)
+        
+        
+        AWSServiceManager.default().defaultServiceConfiguration = controlPlaneServiceConfiguration
+        iotManager = AWSIoTManager.default()
+        iot = AWSIoT.default()
+    }
+    
+    func initializeDataPlane(credentialsProvider: AWSCredentialsProvider) {
+       
+        
+        let iotEndPoint = AWSEndpoint(urlString: IOT_ENDPOINT)
+        
+       
+        let iotDataConfiguration = AWSServiceConfiguration(region: AWS_REGION,
+                                                           endpoint: iotEndPoint,
+                                                           credentialsProvider: credentialsProvider)
+        
+        AWSIoTDataManager.register(with: iotDataConfiguration!, forKey: AWS_IOT_DATA_MANAGER_KEY)
+        iotDataManager = AWSIoTDataManager(forKey: AWS_IOT_DATA_MANAGER_KEY)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    func mqttEventCallbackWebsocket(_ status: AWSIoTMQTTStatus) {
+        guard case .connected = status else {
+            mqttEventCallback(status)
+            return
+        }
+
+        DispatchQueue.main.async {
+          
+            let iot_sample_vc = Iot_sample_ViewController()
+            iot_sample_vc.mqttStatus = "Connected"
+          
+            self.activityIndicatorView.stopAnimating()
+          
+            self.connected = true
+          
+            self.connectIoTDataWebSocket.setTitle("Disconnect \(self.IOT_WEBSOCKET)", for:UIControl.State())
+            
+            self.logTextView.text = "Connected via websocket"
+            
+            self.connectIoTDataWebSocket.isEnabled = true
+        
+        }
+    }
+    
+    @objc func didTapConnectIoTDataWebSocket(_ sender: UIButton) {
+        sender.isEnabled = false
+        if (connected == false) {
+            handleConnectViaWebsocket()
+        } else {
+            handleDisconnect()
+            DispatchQueue.main.async {
+                sender.setTitle("Connect \(self.IOT_WEBSOCKET)", for:UIControl.State())
+                sender.isEnabled = true
+            }
+        }
+    }
+    
+    func handleConnectViaWebsocket() {
+        self.connectButton.isHidden = true
+        activityIndicatorView.startAnimating()
+        DispatchQueue.main.async {
+            self.logTextView.text = "Connecting (data plane)..."
+        }
+        let uuid = UUID().uuidString
+        // Connect to the AWS IoT data plane service over websocket
+        iotDataManager.connectUsingWebSocket(withClientId: uuid, cleanSession: true, statusCallback: mqttEventCallbackWebsocket(_:))
+    }
+    
+    
+    
+    
 }
 
+extension ViewController {
+    
+    func aws_post() {
+        
+        let IOT_ENDPOINT = "https://a2n4hdipq41ly9-ats.iot.ap-south-1.amazonaws.com"
+        let aws_params : Parameters = [
+        
+        
+            "control": "L",
+            "no" : 1,
+            "state" : 1,
+            "speed" : 1
+            
+        ]
+        test_aws(control: "L", no: 1, state: 1, speed: 1)
+//
+        AF.request(IOT_ENDPOINT, method: .post, parameters: aws_params, encoding: JSONEncoding.default, headers: nil).response { response in
+        }
+        
+    }
+    
+    
+}
